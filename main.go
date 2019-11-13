d1fa55e914196c7342cee240cb9f5ce61193751f
Vladislav Jekimtsev <vljeki@ttu.ee>
2019-11-13 18:24:38 +0200

    add handlefunc in main.go

--- main.go
+++ main.go
@@ -5,16 +5,19 @@ import (
 	"net/http"
 
 	item "github.com/Shangye-space/Item-Service/src/api/item"
+	category "github.com/Shangye-space/Item-Service/src/api/item"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
 
+
 	r := mux.NewRouter().StrictSlash(true)
 
 	http.Handle("/", r)
 	r.HandleFunc("/api/items/", item.Get).Methods("GET")
+	r.HandleFunc("/api/categories/", category.Get).Methods("GET")
 	r.HandleFunc("/api/item/delete/{id}", item.Delete).Methods("GET")
 	r.HandleFunc("/api/item/update/{id}", item.Update).Methods("POST")
 	r.HandleFunc("/api/item/create/", item.Create).Methods("POST")
